193c9623cc071709ecbad97cb7ea09096b9312b0
April Kiser <denise626@hotmail.com>
2018-05-22 01:29:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 function getFirstSelector(selector){
-  return document.querySelector(selector)
+  return document.querySelector(selector);
 
 }
 
 function nestedTarget(){
-  return document.getElementById('nested').querySelector('div.target')
+  return document.getElementById('nested').querySelector('div.target');
 
 }
 function increaseRankBy(n){
-  var rank = document.getElementById('app').querySelectorAll('ul.ranked-list')
+  var rank = document.getElementById('app').querySelectorAll('ul.ranked-list');
 
   for (let i = 0; i < rank.length; i++){
-    rank[i].innerHTML = ((parseInt(rank[i].innerHTML)) + n)
+    rank[i].innerHTML = ((parseInt(rank[i].innerHTML)) + n);
     }
 
 }
 
 function deepestChild(){
-  var deepest = document.getElementById('grand-node').querySelectorAll('div')
-  return deepest[deepest.length-1]
+  var deepest = document.getElementById('grand-node').querySelectorAll('div');
+  return deepest[deepest.length-1];
 
 }
